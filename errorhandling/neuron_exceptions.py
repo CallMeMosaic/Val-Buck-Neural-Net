d9a58fb73bcf5438346f6cf3d1bbec06de8769e0
CallMeMosaic <Mosaic@mail.rawmosaic.com>
2026-09-16 23:09:07 +0200

    neuron_exceptions.py: Added Custom Error for DNA Mismatch, as this will likely be necessary more often.

--- errorhandling/neuron_exceptions.py
+++ errorhandling/neuron_exceptions.py
@@ -1,2 +1,14 @@
 class NotEnoughCharge(Exception):
     pass
+
+class NeuronError(Exception):
+    pass
+
+class NeuronDNAError(NeuronError):
+    """Raised when there is a mismatch between DNA data and actual data"""
+
+    def __init__(self, module:type, expected:type, actual:type):
+        module = module
+        expected = expected
+        actual = actual
+        super().__init__(f"DNA mismatch: {module.__name__} expects: {expected.__name__}, but got: {actual.__name__}")
